0d1000d50c5f8d341e2120bb555e4cb722cf0e9b
smartbuddy@gmail.com <smartbuddy@gmail.com>
2019-08-31 17:48:24 +0000

    Added some ant project

---
+++ antproj/project1/src/com/main/Main.java
@@ -0,0 +1,8 @@
+package com.main;
+class Main
+{
+    public static void main(String[] args) {
+       System.out.println("1st argument: "+args[0]);
+        System.out.println("Hello world");
+    }
+}
---
+++ antproj/src/main/Hello.java
@@ -0,0 +1,15 @@
+package main;
+//import org.apache.log4j.Logger;
+//import org.apache.log4j.BasicConfigurator;
+
+class Hello
+{
+  //  static Logger logger = Logger.getLogger(Hello.class);
+    public static void main(String[] args) {
+     //   BasicConfigurator.configure();
+        //logger.info("Hello World");          // the old SysO-statement
+	System.out.println("Hello "+args[0]);    
+}
+
+
+}
